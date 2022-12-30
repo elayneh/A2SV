be3bb047b1e423cb900874b4447d60c0dd7c005f
elayneh <betenatefera@gmail.com>
2022-12-30 23:49:46 +0300

    sentence sorter

---
+++ sort_sentence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sentense_sorter(self, sentence):
+        s = sentence.split()
+        n = len(s)
+        sentence = [""] * n
+
+        for c in s:
+            i = int(c[-1])
+            sentence[i - 1] = c[:-1]
+
+        return " ".join(sentence)
+
+if __name__ == "__main__":
+s = Solution()
+print(s.sentense_sorter("this2 is0 the1 sentence3"))
