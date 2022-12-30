6c1b5dc9af527437c82578c8bc915361c79b4c27
elayneh <betenatefera@gmail.com>
2022-12-30 23:50:25 +0300

    sentence sorter

--- sort_sentence.py
+++ sort_sentence.py
@@ -11,5 +11,6 @@ class Solution:
         return " ".join(sentence)
 
 if __name__ == "__main__":
-s = Solution()
-print(s.sentense_sorter("this2 is0 the1 sentence3"))
+
+    sol = Solution()
+    print(sol.sentense_sorter("this2 is0 the1 sentence3"))
