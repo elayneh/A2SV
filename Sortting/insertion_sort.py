2dd582952dcfc4bee22f167b9314b63cbc43dfc8
elayneh <betenatefera@gmail.com>
2023-01-05 11:11:52 +0300

    note added

--- Sortting/insertion_sort.py
+++ Sortting/insertion_sort.py
@@ -1,16 +1,21 @@
 # usr/bin/python3
 from random import randint
 
-
+"""is used when the data is nearly sorted or when the input size is small
+worst case complexity: O(n2)
+best case complexity: n
+"""
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         target = arr[i]
         hole = i
+        print(arr)
         while hole > 0 and target < arr[hole - 1]:
             arr[hole] = arr[hole - 1]
             hole -= 1
 
         arr[hole] = target
+        print(arr)
 
     return arr
 
