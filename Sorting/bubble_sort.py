375978f27fc7436a0ec477d7349c3fc6b14be080
elayneh <betenatefera@gmail.com>
2023-01-14 23:12:55 +0300

    buble sort implementation added

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,4 +1,18 @@
-def buble_sorter(nums):
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(nums):
     swap_counter = 0
     for i in range(len(nums)):
         for j in range(len(nums) - i - 1):
@@ -6,8 +20,9 @@ def buble_sorter(nums):
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
                 swap_counter = swap_counter + 1
 
-    print("Array is sorted in {} swaps\nFirst Element: {}\nLast Element: {}".format(swap_counter, nums[0], nums[len(nums) - 1]))
+    print("Array is sorted in {} swaps.\nFirst Element: {}\nLast Element: {}".format(swap_counter, nums[0], nums[-1]))
 
-if __name__ == "__main__":
-    arr = [6,4,1]
-    buble_sorter(arr)
+if __name__ == '__main__':
+    n = int(input().strip())
+    a = list(map(int, input().rstrip().split()))
+    countSwaps(a)
