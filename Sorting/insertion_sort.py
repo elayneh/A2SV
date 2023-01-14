24eb43303446484f4f9a99c64f57440914284f8b
elayneh <betenatefera@gmail.com>
2023-01-14 11:53:31 +0300

    solution modified

--- Sorting/insertion_sort.py
+++ Sorting/insertion_sort.py
@@ -1,4 +1,4 @@
-# usr/bin/python3
+#!/usr/bin/python3
 from random import randint
 
 """is used when the data is nearly sorted or when the input size is small
@@ -6,21 +6,15 @@ worst case complexity: O(n2)
 best case complexity: n
 """
 def insertion_sort(arr):
-    for i in range(1, len(arr)):
-        target = arr[i]
-        hole = i
-        print(arr)
-        while hole > 0 and target < arr[hole - 1]:
-            arr[hole] = arr[hole - 1]
-            hole -= 1
+   for i in range(len(arr)):
+        target = i
+        while arr[target] < arr[target - 1] and target > 0:
+            arr[target], arr[target - 1] = arr[target - 1], arr[target]
+            target -= 1
 
-        arr[hole] = target
-        print(arr)
-
-    return arr
+        if i > 0:
+            print(*arr)
 
 if __name__ == "__main__":
     original_array = arr = [randint(10, 100) for i in range(10)]
-    print("Original array: {}".format(original_array))
-    sorted_array = insertion_sort(arr)
-    print("Sorted array: {}".format(sorted_array))
+    insertion_sort([3, 4, 7, 5, 6, 2, 1])
