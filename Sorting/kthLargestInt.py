31c4fcaeb5563485b4cf6fd3a5c89fbe3d12bb21
elayneh <betenatefera@gmail.com>
2023-01-18 09:06:08 +0300

    modified for minizing time complexity

--- Sorting/kthLargestInt.py
+++ Sorting/kthLargestInt.py
@@ -1,11 +1,18 @@
 class Solution:
     def kthLargestNumber(self, nums: list, k: int)-> str:
-        for i in range(len(nums)):
-            nums[i] = int(nums[i])
+        nums = [int(item) for item in nums]
 
-        for i in range(len(nums)):
-            for j in range(len(nums)- i - 1):
-                if(nums[j] > nums[j + 1]):
-                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)- i - 1):
+        #         if(nums[j] > nums[j + 1]):
+        #             nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        nums.sort()
 
+        print(nums)
         return "{}".format(str(nums[-k]))
+
+
+
+
+s = Solution()
+s.kthLargestNumber([34,2,4,2,54], 3)
