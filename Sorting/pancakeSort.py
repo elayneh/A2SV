08920212cdedec94f9edc0d7015f7e5015511174
elayneh <betenatefera@gmail.com>
2023-01-19 01:10:59 +0300

    modified

--- Sorting/pancakeSort.py
+++ Sorting/pancakeSort.py
@@ -2,8 +2,8 @@ class Solution:
     def pancakeSort(self, arr: list[int]) -> list[int]:
         res = []
         for item in range(len(arr), 1, -1):
-            idx = arr.index(item)
-            res.extend([idx + 1, item])
+            idx = arr.index(item) # to get the indext of the list item
+            res.extend([idx + 1, item]) # to sum up list to list res
             arr = arr[:idx:-1] + arr[:idx]
         return res
 
