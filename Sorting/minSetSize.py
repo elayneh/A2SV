ae0c085fd72c6003ebc8d8bfe9204e61e3adeea8
elayneh <betenatefera@gmail.com>
2023-01-19 00:37:59 +0300

    modified

--- Sorting/minSetSize.py
+++ Sorting/minSetSize.py
@@ -5,25 +5,27 @@ remove all the occurences of these integers in the array.
 Return the minimum size the set so that at least half of the integers of the array are removed.
 """
 
-from collections import Counter
+from collections import Counter # A Counter is a dict subclass for counting hashable objects.
+                                # It is a collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
 
-print(Counter([2,2,2,3,3,3,4,4,4]))
+
 class Solution:
     def minSetSize(self, arr: list[int]) -> int:
 
         counter = Counter(arr)
         freq = sorted(counter.values(), reverse=True)
+        print(freq)
 
         half_size = len(arr) // 2
-        res = 0
+        noSets = 0
 
         while half_size > 0:
-            half_size -= freq[res]
-            res += 1
+            half_size -= freq[noSets]
+            noSets += 1
 
-        return res
+        return noSets
 
 
 
 s = Solution()
-print(s.minSetSize([3,3,3,3,3,3,3,3,3]))
+print(s.minSetSize([3,3,3,3,5,5,5,2,2,7]))
