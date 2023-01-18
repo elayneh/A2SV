db7ccf5a6fbab01cb70cb4c10c9c7348f5c0e325
elayneh <betenatefera@gmail.com>
2023-01-18 23:56:08 +0300

    modified

--- Sorting/checkArthmeticSubArray.py
+++ Sorting/checkArthmeticSubArray.py
@@ -21,17 +21,21 @@ class Solution:
     def checkArithmeticSubarrays(self, nums: list[int], l: list[int], r: list[int]) -> list[bool]:
         d = {}
         res = []
-
         for i in range(len(l)):
-            d[i] = nums[l[i]:r[i]+1]
-        for item in d:
+            d[i] = nums[l[i]: r[i] + 1]
+    
+        for i in range(len(d)):
             j = 0
-            d[item].sort()
-            for i in range(2,len(d[item])):
-                if d[item][i]-d[item][i-1]!=d[item][1]-d[item][0]:
+            d[i].sort()
+    
+            for k in range(2, len(d[i])):
+                if d[i][k] - d[i][k - 1] != d[i][1] - d[i][0]:
                     res.append(False)
                     break
-                j+=1
-            if j==len(d[item])-2:
+                
+                j += 1
+    
+            if j == len(d[i]) - 2:
                 res.append(True)
+    
         return res
