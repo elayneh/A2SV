2b8adfaca73ee79da65fc1719c577f0488042fbd
elayneh <betenatefera@gmail.com>
2023-01-19 00:29:39 +0300

    modified

--- Sorting/minSetSize.py
+++ Sorting/minSetSize.py
@@ -7,18 +7,18 @@ Return the minimum size the set so that at least half of the integers of the arr
 
 from collections import Counter
 
-
+print(Counter([2,2,2,3,3,3,4,4,4]))
 class Solution:
     def minSetSize(self, arr: list[int]) -> int:
 
         counter = Counter(arr)
-        noFreq = sorted(counter.values(), reverse=True)
+        freq = sorted(counter.values(), reverse=True)
 
         half_size = len(arr) // 2
         res = 0
 
         while half_size > 0:
-            half_size -= noFreq[res]
+            half_size -= freq[res]
             res += 1
 
         return res
