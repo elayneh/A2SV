683f1d19c9b17848c4270cd4f26f3d6b2b95efc6
elayneh <betenatefera@gmail.com>
2023-01-19 01:08:21 +0300

    solution to sort pancake

--- Sorting/pancakeSort.py
+++ Sorting/pancakeSort.py
@@ -1,12 +1,11 @@
 class Solution:
     def pancakeSort(self, arr: list[int]) -> list[int]:
         res = []
-        for i in range(len(arr), 1, -1):
-            idx = arr.index(i)
-            res.extend([idx + 1, i])
+        for item in range(len(arr), 1, -1):
+            idx = arr.index(item)
+            res.extend([idx + 1, item])
             arr = arr[:idx:-1] + arr[:idx]
         return res
-
 
 s =Solution()
 print(s.pancakeSort([1,2,3]))
