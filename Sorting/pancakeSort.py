0c2a4044f775d46fbbe61178860299e41ad46c25
elayneh <betenatefera@gmail.com>
2023-01-19 01:12:29 +0300

    problem description added

--- Sorting/pancakeSort.py
+++ Sorting/pancakeSort.py
@@ -1,3 +1,17 @@
+"""
+Given an array of integers arr, sort the array by performing a series of pancake flips.
+
+In one pancake flip we do the following steps:
+
+Choose an integer k where 1 <= k <= arr.length.
+Reverse the sub-array arr[0...k-1] (0-indexed).
+
+Return an array of the k-values corresponding to a sequence of pancake flips that sort arr. Any valid answer that sorts the array within 10 * arr.
+length flips will be judged as correct.
+
+
+"""
+
 class Solution:
     def pancakeSort(self, arr: list[int]) -> list[int]:
         res = []
