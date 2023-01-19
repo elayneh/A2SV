df415a1d1db52f62ec1919d302b377ff3c6186cf
elayneh <betenatefera@gmail.com>
2023-01-19 13:52:46 +0300

    solution for minimized pair sum is added

---
+++ Sorting/minPairSum.py
@@ -0,0 +1,19 @@
+"""
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+"""
+class Solution:
+    def minPairSum(self, nums: list[int])-> int:
+        sorted_arr = sorted(nums)
+        reversed_sorted_arr = sorted(nums)[::-1]
+        sum = []
+
+        for a, b in zip(sorted_arr, reversed_sorted_arr):
+            sum.append(a+b)
+
+        return max(sum)
