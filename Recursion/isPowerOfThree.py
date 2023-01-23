b28eeb1fdae489c3bb387c2a97fce6330affef38
elayneh <betenatefera@gmail.com>
2023-01-23 09:27:53 +0300

    solution to check is n is a power of 3

---
+++ Recursion/isPowerOfThree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+"""
+class Solution():
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        elif n % 3 == 0:
+            return self.isPowerOfThree(n // 3)
+        if n == 1:
+            return True
+        return False
