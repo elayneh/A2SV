c5518b967737e0b2dac840feec48b18f9e491582
elayneh <betenatefera@gmail.com>
2023-01-25 20:54:42 +0300

    solution to remove element is added

---
+++ TwoPointer/removeElement.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+"""
+
+class Solution():
+    def removeElement(self, nums: list[int], val: int) -> list[int]:
+        if len(nums) == 0:
+            return 0
+        ptr = 0
+        for num in nums:
+            if num != val:
+                nums[ptr] = num
+                ptr += 1
+
+
+        return ptr
