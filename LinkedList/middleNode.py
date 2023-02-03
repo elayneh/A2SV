4d09c832a54d2ad68d5172dea15ac3a2fc63140b
elayneh <betenatefera@gmail.com>
2023-02-04 01:32:32 +0300

    python3 implementation for middleNode problem

---
+++ LinkedList/middleNode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the secodnd middle node.
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fastPtr = slowPtr = head
+        while slowPtr and fastPtr and fastPtr.next:
+            slowPtr = slowPtr.next
+            fastPtr = slowPtr.next.next
+
+        return slowPtr
