74a34083b63bdd2adb505fc72b9ef0d600cc1205
belayneh getachew <87185500+elayneh@users.noreply.github.com>
2023-02-10 20:35:29 +0300

    Delete prefixSum directory

--- prefixSum/numberOfSubarrays.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
-
-Return the number of nice sub-arrays.
-"""
-class Solution:
-    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        ...
