835d54b738025bb47504fb572d6f6145b0eb2ad8
elayneh <betenatefera@gmail.com>
2023-02-16 11:01:53 +0300

    modified

--- WindowSliding/max_subarray.py
+++ WindowSliding/max_subarray.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
 
-def maxSum(l, n, k):
-    window_sum = sum(l[:k])
-    max_sum = window_sum
+def maxSumArray(nums: list[int], k: int) -> int:
 
-    for i in range(n - k):
-        window_sum =  window_sum - l[i] + l[k + i]
-        max_sum = max(max_sum, window_sum)
+    win_sum = sum(nums[:k])
+    max_sum = win_sum
 
+    for i in range(len(nums) - k):
+            win_sum += nums[k + i] - nums[i]
+            max_sum = max(max_sum, win_sum)
 
     return max_sum
 
-arr = [1, 4, 2, 10, 2,
-       3, 1, 0, 20]
-n = len(arr)
 
-print(maxSum(arr, n, 4))
-
+print(maxSumArray([21,3,6,76,4,56], 2))
