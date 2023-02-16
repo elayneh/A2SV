227a656e8b430cd1889e8c24849384afc3c5e18c
elayneh <betenatefera@gmail.com>
2023-02-16 11:01:13 +0300

    modified

--- LinkedList/addTwoNumbers.py
+++ LinkedList/addTwoNumbers.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order, and each of their nodes contains a single digit.
+Add the two numbers and return the sum as a linked list.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
@@ -29,9 +31,8 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
-            if l1 or l2 or carry != 0:
+            if l1 or l2 or carry:
                 curr.next = ListNode(carry)
                 curr = curr.next
-
         return node
 
